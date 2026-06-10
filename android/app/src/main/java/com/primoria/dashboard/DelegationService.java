a124ca64c35549f618a1053915b9138b489e4686
ZhangJunQiu-Alan <alanmuskccoder@gmail.com>
2026-06-11 02:33:03 +0800

    Add Android APK packaging

---
+++ android/app/src/main/java/com/primoria/dashboard/DelegationService.java
@@ -0,0 +1,13 @@
+package com.primoria.dashboard;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
